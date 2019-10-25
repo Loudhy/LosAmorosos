67a643fd8627c6431fbdfbf4190929792f102fdc
alulab14 <alulab14@inf.pucp.edu.pe>
2019-10-25 12:53:29 -0500

    Agregar presentacion y producto_presentacion

--- EduktSoftController/src/mysql/UsuarioMySQL.java
+++ EduktSoftController/src/mysql/UsuarioMySQL.java
@@ -30,8 +30,8 @@ public class UsuarioMySQL implements UsuarioDAO {
         int resultado = 0;
         try{       
             con = DriverManager.getConnection(DBManager.url, DBManager.user, DBManager.password);
-            cs = con.prepareCall("{call INSERTAR_USUARIO(?,?,?,?,?)} ");
-            //cs.setInt("_ID_EMPLEADO", usuario.getEmpleado().getId());
+            cs = con.prepareCall("{call INSERTAR_USUARIO(?,?,?,?)} ");
+            cs.setInt("_ID_EMPLEADO", usuario.getEmpleado().getId());
             cs.setString("_NOMBRE_USUARIO", usuario.getNombre());
             cs.setString("_CONTRASEÑA", usuario.getContraseña());
             cs.setBoolean("_ACTIVE", usuario.isActive());
